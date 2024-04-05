98e800b17f0be48d706acbca317d9b6fb6bf7beb
Dima Pavlov <mitrijpavlov@gmail.com>
2024-04-05 09:17:34 +0300

    Update linter config, move fixtures

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,11 +13,11 @@ def run_parser():
     parser.parse_args()
 
 
-def generate_diff(file1, file2):
+def generate_diff(file1: dict, file2: dict) -> dict:
 
     # Calculate difference
     diff = {}
-    for key in sorted(file1.keys())+sorted(file2.keys()):
+    for key in set(sorted(file1.keys())+sorted(file2.keys())):
         if key not in file2:
             diff[f'- {key}'] = file1[key]
         elif key not in file1:
@@ -30,11 +30,8 @@ def generate_diff(file1, file2):
                 diff[key] = file1[key]
 
     # Print the {diff} in a readable format in the console
-    printable_result = []
     for key, value in diff.items():
-        printable_result.append(f"{key}: {value}")
-    for i in printable_result:  # TODO: No need for the second for loop here, just print key and value in the first loop
-        print(i)
+        print(f"{key}: {value}")
 
     # Return result
     return diff
@@ -43,9 +40,23 @@ def generate_diff(file1, file2):
 def main():
     print('')
     print("Hello, %username%! Be assured, the Gendiff script is running!")
-    # file1 = json.load(open('file1.json'))
-    # file2 = json.load(open('file2.json'))
-    # generate_diff(file1, file2)
+    #file1 = json.load(open('tests/fixtures/file1.json'))
+    #file2 = json.load(open('tests/fixtures/file2.json'))
+
+    file1 = {
+        "host": "hexlet.io",
+        "timeout": 50,
+        "proxy": "123.234.53.22",
+        "follow": False
+    }
+
+    file2 = {
+            "timeout": 20,
+            "verbose": True,
+            "host": "hexlet.io"
+    }
+
+    generate_diff(file1, file2)
     run_parser()
 
 
